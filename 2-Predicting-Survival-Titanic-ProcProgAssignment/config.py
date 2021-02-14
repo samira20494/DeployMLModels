62869a975393a01750abb9be351fa569b6bd92bf
Samira Alizadehzoj <samira.alizadehzoj@gmail.com>
2021-02-14 18:53:10 -0500

    reproduce the model with the procedural programming scripts

--- 2-Predicting-Survival-Titanic-ProcProgAssignment/config.py
+++ 2-Predicting-Survival-Titanic-ProcProgAssignment/config.py
@@ -8,20 +8,29 @@ OUTPUT_MODEL_PATH = 'logistic_regression.pkl'
 # ======= PARAMETERS ===============
 
 # imputation parameters
-IMPUTATION_DICT = 
+IMPUTATION_DICT = []
 
 
 # encoding parameters
-FREQUENT_LABELS = 
+FREQUENT_LABELS = {
+    'sex': ['female', 'male'],
+    'cabin': ['C', 'Missing'],
+    'embarked': ['C', 'Q', 'S'],
+    'title': ['Miss', 'Mr', 'Mrs']}
 
 
-DUMMY_VARIABLES = 
+DUMMY_VARIABLES = {
+    'sex': ['sex_male'],
+    'cabin': ['cabin_Missing', 'cabin_Rare'],
+    'embarked': ['embarked_Q', 'embarked_Rare', 'embarked_S'],
+    'title': ['title_Mr', 'title_Mrs', 'title_Rare']}
 
 
 # ======= FEATURE GROUPS =============
 
 TARGET = 'survived'
 
-CATEGORICAL_VARS = 
+CATEGORICAL_VARS = ['sex', 'cabin', 'embarked', 'title']
 
-NUMERICAL_TO_IMPUTE = 
+
+NUMERICAL_TO_IMPUTE = ['age', 'fare']
--- 2-Predicting-Survival-Titanic-ProcProgAssignment/preprocessing_functions.py
+++ 2-Predicting-Survival-Titanic-ProcProgAssignment/preprocessing_functions.py
@@ -13,77 +13,85 @@ import joblib
 
 def load_data(df_path):
     # Function loads data for training
-    pass
-
+    return pd.read_csv(df_path)
 
 
 def divide_train_test(df, target):
     # Function divides data set in train and test
-    pass
-    
+    X_train, X_test, y_train, y_test = train_test_split(
+        df.drop(target, axis=1),  # predictors
+        df[target],  # target
+        test_size=0.2,  # percentage of obs in test set
+        random_state=0)  # seed to ensure reproducibility
 
+    return X_train, X_test, y_train, y_test
 
 
 def extract_cabin_letter(df, var):
     # captures the first letter
-    pass 
-
+    return df[var].str[0]
 
 
 def add_missing_indicator(df, var):
     # function adds a binary missing value indicator
-    pass
+    df[var+'_NA'] = np.where(df[var].isnull(), 1, 0)
+    return df
 
 
-    
-def impute_na():
+def impute_na(df, var, value='Missing'):
     # function replaces NA by value entered by user
     # or by string Missing (default behaviour)
-    pass
+    return df[var].fillna(value)
 
 
-
-def remove_rare_labels():
+def remove_rare_labels(df, var, frequent_labels):
     # groups labels that are not in the frequent list into the umbrella
     # group Rare
-    pass
-
+    return np.where(df[var].isin(frequent_labels), df[var], 'Rare')
 
 
 def encode_categorical(df, var):
     # adds ohe variables and removes original categorical variable
-    
-    df = df.copy()
-    
-    pass
 
+    df = df.copy()
+    df = pd.concat([df, pd.get_dummies(df[var], prefix=var, drop_first=True)], axis=1)
+    df.drop(labels=var, axis=1, inplace=True)
+    return df
 
 
 def check_dummy_variables(df, dummy_list):
-    
     # check that all missing variables where added when encoding, otherwise
     # add the ones that are missing
-    pass
-    
+    for d in dummy_list:
+        if d not in df.columns:
+            df[d] = 0
+
+    return df
+
 
 def train_scaler(df, output_path):
     # train and save scaler
-    pass
-  
-    
+    scaler = StandardScaler()
+    scaler.fit(df)
+    joblib.dump(scaler, output_path)
+    return scaler
+
 
 def scale_features(df, output_path):
     # load scaler and transform data
-    pass
-
+    scaler = joblib.load(output_path)  # with joblib probably
+    return scaler.transform(df)
 
 
 def train_model(df, target, output_path):
     # train and save model
-    pass
+    model = LogisticRegression(C=0.0005, random_state=0)
+    model.fit(df, target)
+    joblib.dump(model, output_path)
 
+    return None
 
 
 def predict(df, model):
-    # load model and get predictions
-
+    model = joblib.load(model)
+    return model.predict(df)
